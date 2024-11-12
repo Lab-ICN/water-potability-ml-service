ca06ea15b6fb48c0e77c8850563b94d2dd4f909b
ghazafm <fauzan.ghaza@gmail.com>
2024-11-12 19:44:16 +0700

    fix merge

---
+++ Deployment/client.py
@@ -0,0 +1,30 @@
+import grpc
+import water_potability_pb2_grpc as pb2_grpc
+import water_potability_pb2 as pb2
+
+class WaterClient(object) :
+    
+    def __init__(self):
+        self.host = 'localhost'
+        self.server_port = 50051
+        
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(self.host, self.server_port)
+        )
+        
+        self.stub = pb2_grpc.WaterPotabilityServiceStub(self.channel)
+    
+    def predict(self) :
+        this_message = pb2.PredictWaterPotabilityRequest(
+            id = '1',
+            ph = 2,
+            totalDissolveSolids = 3,
+            turbidity = 4
+            )
+        return self.stub.PredictWaterPotability(this_message)
+    
+if __name__ == '__main__' :
+    client = WaterClient()
+    res = client.predict()
+    print(res)
+        
